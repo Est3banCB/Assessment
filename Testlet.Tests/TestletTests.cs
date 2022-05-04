2c47da0b841da01a6f20856931c98c7ce57ab8d0
Esteban Cordero Benavides <estebancb@hotmail.com>
2022-05-03 21:42:00 -0600

    Test creation 2.1

--- Testlet.Tests/TestletTests.cs
+++ Testlet.Tests/TestletTests.cs
@@ -37,5 +37,30 @@ namespace Testlet.Tests
             var ex = Assert.Throws<ArgumentNullException>(() => new Testlet(Guid.NewGuid().ToString(), null));
             Assert.AreEqual(expectedErrorMessage, ex.Message);
         }
+
+        [Test]
+        public void InvalidNumberOfOperational()
+        {
+            var operational = new Item()
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                ItemType = Enums.ItemTypeEnum.Operational
+            };
+
+            var pretest = new Item()
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                ItemType = Enums.ItemTypeEnum.Pretest
+            };
+
+            var lstItems = new List<Item>()
+            {
+                operational, pretest, pretest, pretest, pretest
+            };
+
+            var expectedErrorMessage = "The number of items of type Operational is not correct.";
+            var ex = Assert.Throws<ArgumentException>(() => new Testlet(Guid.NewGuid().ToString(), lstItems));
+            Assert.AreEqual(expectedErrorMessage, ex.Message);
+        }
     }
 }
